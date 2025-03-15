e21256b710d7af3a7a87139b94c4b3a078733808
NATTAPONG\Jonus.co.th <Nattapong_Jonus@outlook.com>
2025-03-15 10:19:45 +0700

    Add serialization import to security_manager.py for enhanced key handling

--- thainlp/security/security_manager.py
+++ thainlp/security/security_manager.py
@@ -22,6 +22,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 
 class InputValidator:
     """Validate and sanitize input"""
